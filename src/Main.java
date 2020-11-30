646e376ac3d1944c7208b7fc66e05f790e55bfde
Emanuele Mottola <emanuele.mottola94@gmail.com>
2020-11-30 18:25:22 +0100

    checkPermutation completed

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         hasUniqueValues("Help me handle the coding interview!");
+        checkPermutation("code is awsome!", "some code is aw!");
     }
 
     /*
-    Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
-        cannot use additional data structures?
+    Ex 1.1
      */
     public static boolean hasUniqueValues(@NotNull String str){
 
@@ -23,6 +23,22 @@ public class Main {
                 return true;
         }
         return false;
+    }
+
+    /*
+     Ex 1.2
+    */
+    public static boolean checkPermutation(String a, String b){
+        if (a.length() != b.length())
+            return false;
+
+        char[] a_copy = a.toCharArray();
+        char[] b_copy = b.toCharArray();
+
+        Arrays.sort(a_copy);
+        Arrays.sort(b_copy);
+
+        return Arrays.equals(a_copy, b_copy);
     }
 
 }
